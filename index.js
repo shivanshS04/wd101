28f65c92b7b6a3083f654b6ff94bfc47647b4f61
shivanshS04 <65936350+shivanshS04@users.noreply.github.com>
2024-09-08 08:09:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ var entries = localStorage.getItem("entries")
   ? JSON.parse(localStorage.getItem("entries"))
   : [];
 var res = {};
+function updateTable() {
+  let entries_table = document.getElementById("entries_table");
+  for (let i = 1; i < entries_table.rows.length; i++) {
+    entries_table.deleteRow(i);
+  }
+  entries.map((entry, index) => {
+    let row = document.createElement("tr");
+    Object.keys(entry).forEach((item) => {
+      let cell = document.createElement("td");
+      if (item == "agreed") {
+        cell.textContent = entry[item] == "on" ? true : false;
+      } else {
+        cell.textContent = entry[item];
+      }
+      cell.className = "p-2 border-b border-l text-left";
+      row.appendChild(cell);
+    });
+    entries_table.appendChild(row);
+  });
+}
+updateTable();
 document.getElementById("reg_form").addEventListener("submit", function (e) {
   e.preventDefault();
   const data = new FormData(document.getElementById("reg_form"));
@@ -10,22 +31,5 @@ document.getElementById("reg_form").addEventListener("submit", function (e) {
   }
   entries.push(res);
   localStorage.setItem("entries", JSON.stringify(entries));
-  location.reload();
-});
-
-let entries_table = document.getElementById("entries_table");
-
-entries.map((entry, index) => {
-  let row = document.createElement("tr");
-  Object.keys(entry).forEach((item) => {
-    let cell = document.createElement("td");
-    if (item == "agreed") {
-      cell.textContent = entry[item] == "on" ? true : false;
-    } else {
-      cell.textContent = entry[item];
-    }
-    cell.className = "p-2 border-b border-l text-left";
-    row.appendChild(cell);
-  });
-  entries_table.appendChild(row);
+  updateTable();
 });
